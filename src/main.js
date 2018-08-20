123ef478cf1c437c55864ba32c8a8f763773425c
feeling <540449517@qq.com>
2018-08-20 18:24:33 +0800

    备份

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-document.write("hello world")
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './app.vue'
+import routerConfig from './router'
+Vue.use(VueRouter)
+const router = new VueRouter(routerConfig)
+new Vue({
+    el:"#app",
+    router:router,
+    render:h=>h(App)//h->createElement 渲染html
+})
---
+++ src/router.js
@@ -0,0 +1,6 @@
+import index from './views/index.vue'
+export default{
+    routes:[
+        {path:'/index',component:index}
+    ]
+}
